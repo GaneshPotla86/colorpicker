a87ba2496d47b3dc8669dda9090eeaf2ca430c9f
GaneshPotla86 <potlaganesh563@gmail.com>
2025-11-27 11:31:23 +0530

    javascript code is added

---
+++ script.js
@@ -0,0 +1,23 @@
+function button1() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").style.color = "blue";
+}
+
+function button2() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+    document.getElementById("selectedColorHexCode").style.color = "blue";
+}
+
+function button3() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+    document.getElementById("selectedColorHexCode").style.color = "blue";
+}
+
+function button4() {
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").style.color = "blue";
+}
